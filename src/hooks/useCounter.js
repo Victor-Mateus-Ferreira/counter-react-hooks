87d88d372960fb4afb405212977dd8e12415944f
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-11-17 11:39:03 -0300

    create context API

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 
 const useCounter = () => {
-  const [numberShuffle] = useState(0);
-  useEffect(() => {
-    setInterval(() => {numberShuffle === Math.floor(Math.random() * (100 - 1) + 1 )}, 10000)
-  },[numberShuffle])
-  return numberShuffle;
+  const [numberShuffle, setNumberShuffle] = useState(0);
+  return useEffect(() => {
+    setInterval(() => setNumberShuffle(Math.floor(Math.random() * (100 - 1) + 1 ), 10000))
+  },[numberShuffle]);
 }
 
 export default useCounter;
